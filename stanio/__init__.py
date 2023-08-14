64bc2f0404af75e9b30328576d6dbaae78a9c60b
Brian Ward <bward@flatironinstitute.org>
2023-08-14 12:26:02 -0400

    Update metadata

--- stanio/__init__.py
+++ stanio/__init__.py
@@ -3,3 +3,5 @@ from .json import write_stan_json
 from .reshape import Parameter, parse_header, stan_variables
 
 __all__ = ["read_csv", "write_stan_json", "Parameter", "parse_header", "stan_variables"]
+
+__version__ = "0.1.0"
